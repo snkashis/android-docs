8f26692df5dd7c8e0b775c1deabdece675d61b50
Cameron Mace <cameron@mapbox.com>
2017-03-23 11:05:43 -0400

    added more redirect

--- pages/mapbox-java/index.js
+++ pages/mapbox-java/index.js
@@ -5,7 +5,7 @@ import { prefixLink } from 'gatsby-helpers'
 
 module.exports = React.createClass({
   componentDidMount: function() {
-    window.location.href = prefixLink("/map-sdk/5.0.1/getting-started/")
+    window.location.href = window.location.href = prefixLink("/mapbox-java/2.0.0/getting-started/")
   },
   render () {
     return (<div></div>
