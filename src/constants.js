da6ec3d46b47f1bddfe54377dc8ef3fb6389c874
Langston Smith <langston.smith@mapbox.com>
2017-09-01 14:15:52 -0700

    bumped to 0.5.0 (#204)
    * bumped to 0.5.0
    
    * added javadocs

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 /* SDK Versions */
 export const MAS_VERSION = '2.2.3';
 export const MAP_SDK_VERSION = '5.1.3';
-export const NAVIGATION_VERSION = '0.4.0';
+export const NAVIGATION_VERSION = '0.5.0';
 export const TRAFFIC_PLUGIN_VERSION = '0.3.0';
 export const LOCATION_LAYER_PLUGIN_VERSION = '0.1.0';
 export const BUILDING_PLUGIN_VERSION = '0.1.0';
