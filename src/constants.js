3a649cd904605d4638a00d8a98fdb1ed9b9eac6f
Langston Smith <langston.smith@mapbox.com>
2017-10-09 13:19:43 -0700

    Android MAS 2.2.6 adjustments (#236)
    * bumped to 2.2.6 in constants file
    
    * added 2.2.6 javadocs

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 /* SDK Versions */
-export const MAS_VERSION = '2.2.5';
+export const MAS_VERSION = '2.2.6';
 export const MAP_SDK_VERSION = '5.2.0-beta.1';
 export const NAVIGATION_VERSION = '0.6.0';
 export const TRAFFIC_PLUGIN_VERSION = '0.3.0';
