ea71cc5f93d279014c0cc161cd7862d40f1716b8
Langston Smith <langston.smith@mapbox.com>
2017-10-27 10:12:59 -0700

    5.2.0-beta.3 javadocs addition and constants bump (#242)
    * bumped to beta.3 in constants file
    
    * added 5.2.0-beta.3 javadocs

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 /* SDK Versions */
 export const MAS_VERSION = '2.2.6';
-export const MAP_SDK_VERSION = '5.2.0-beta.2';
+export const MAP_SDK_VERSION = '5.2.0-beta.3';
 export const NAVIGATION_VERSION = '0.6.2';
 export const TRAFFIC_PLUGIN_VERSION = '0.3.0';
 export const LOCATION_LAYER_PLUGIN_VERSION = '0.1.0';
