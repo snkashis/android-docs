e0c23ff1a0e9e6f4eb7f85982e93457d85555083
Langston Smith <langston.smith@mapbox.com>
2017-12-07 16:39:24 -0800

    5.2.1 adjustments (version bump, gradle 3.0, & docs) (#274)
    * bumped to 5.2.1
    
    * adjusted dependency line
    
    * added javadocs

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 /* SDK Versions */
 export const MAS_VERSION = '2.2.9';
-export const MAP_SDK_VERSION = '5.2.0';
+export const MAP_SDK_VERSION = '5.2.1';
 export const NAVIGATION_VERSION = '0.7.1';
 export const TRAFFIC_PLUGIN_VERSION = '0.3.0';
 export const LOCATION_LAYER_PLUGIN_VERSION = '0.2.0';
