b9660d970c2fb26170437165b599bd1b5ec484aa
Langston Smith <langston.smith@mapbox.com>
2018-01-23 08:28:40 -0800

    Bumped maps sdk to 5.3.2 + javadocs (#290)
    * bumped to 5.3.2
    
    * added javadocs

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 /* SDK Versions */
 export const MAS_VERSION = '2.2.9';
-export const MAP_SDK_VERSION = '5.3.1';
+export const MAP_SDK_VERSION = '5.3.2';
 export const NAVIGATION_VERSION = '0.8.0';
 export const TRAFFIC_PLUGIN_VERSION = '0.3.0';
 export const PLACES_PLUGIN_VERSION = '0.1.0';
