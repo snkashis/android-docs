da8165a10773cc8e83bde46cba35a7a0bc8220c0
Langston Smith <langston.smith@mapbox.com>
2018-01-24 15:54:07 -0800

    bumped MAS to 3.0.0-beta.2 (#291)
    * bumped to 3.0.0-beta.2
    
    * tweak

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 /* SDK Versions */
-export const MAS_VERSION = '2.2.9';
+export const MAS_VERSION = '3.0.0-beta.2';
 export const MAP_SDK_VERSION = '5.3.2';
 export const NAVIGATION_VERSION = '0.9.0';
 export const TRAFFIC_PLUGIN_VERSION = '0.3.0';
