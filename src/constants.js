32706b7c13e53463bc98f1d3db8c4f923c776e4b
Cameron Mace <cameron@mapbox.com>
2018-02-26 13:25:53 -0500

    Mas 3.0 beta.3 docs (#325)
    * add beta 3 javadoc
    
    * bumped version

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 /* SDK Versions */
-export const MAS_VERSION = '3.0.0-beta.2';
+export const MAS_VERSION = '3.0.0-beta.3';
 export const MAP_SDK_VERSION = '5.4.1';
 export const NAVIGATION_VERSION = '0.9.0';
 export const TRAFFIC_PLUGIN_VERSION = '0.3.0';
