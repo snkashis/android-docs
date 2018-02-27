6c1383d4fb15d40410a619b22854c98ed6eb26dd
Langston Smith <langston.smith@mapbox.com>
2018-02-27 14:02:57 -0800

    added localization plugin constant to constants list (#335)

--- src/constants.js
+++ src/constants.js
@@ -7,4 +7,5 @@ export const PLACES_PLUGIN_VERSION = '0.2.1';
 export const LOCATION_LAYER_PLUGIN_VERSION = '0.4.0';
 export const BUILDING_PLUGIN_VERSION = '0.1.0';
 export const GEOJSON_PLUGIN_VERSION = '0.1.0';
+export const LOCALIZATION_PLUGIN_VERSION = '0.1.0';
 export const STYLE_VERSION = '8';
