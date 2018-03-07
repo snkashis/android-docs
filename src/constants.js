9de8bcefe5516d249d17da77f3fa339f207600a0
Cameron Mace <cameron@mapbox.com>
2018-03-07 16:44:53 -0500

    bumped versions (#350)

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,8 @@
 export const MAS_VERSION = '3.0.0-beta.3';
 export const MAP_SDK_VERSION = '5.5.0';
 export const NAVIGATION_VERSION = '0.11.0';
-export const TRAFFIC_PLUGIN_VERSION = '0.3.0';
-export const PLACES_PLUGIN_VERSION = '0.2.1';
+export const TRAFFIC_PLUGIN_VERSION = '0.4.0';
+export const PLACES_PLUGIN_VERSION = '0.2.2';
 export const LOCATION_LAYER_PLUGIN_VERSION = '0.4.0';
 export const BUILDING_PLUGIN_VERSION = '0.1.0';
 export const GEOJSON_PLUGIN_VERSION = '0.1.0';
