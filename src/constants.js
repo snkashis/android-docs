d2f4ab8a6a17a560b5d5471cf5231410e8bfd7a4
Langston Smith <langston.smith@mapbox.com>
2018-04-02 13:34:10 -0700

    Bumped to 3.0.1 (#393)

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 /* SDK Versions */
-export const MAS_VERSION = '3.0.0';
+export const MAS_VERSION = '3.0.1';
 export const MAP_SDK_VERSION = '5.5.1';
 export const NAVIGATION_VERSION = '0.11.1';
 export const CORE_VERSION = '0.1.1';
