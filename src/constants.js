90313a0ae0a797b9902978262fb1353d5c785d9b
Langston Smith <langston.smith@mapbox.com>
2018-05-07 08:22:31 -0700

    Bumped maps sdk to 6.1.0 (#452)
    * bumped to 6.1.0
    
    * added javadocs

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 /* SDK Versions */
 export const JAVA_SDK_VERSION = '3.1.0';
-export const MAP_SDK_VERSION = '6.0.1';
+export const MAP_SDK_VERSION = '6.1.0';
 export const NAVIGATION_VERSION = '0.13.0';
 export const TELEMETRY_VERSION = '3.0.0-beta.1';
 export const CORE_VERSION = '0.2.0';
