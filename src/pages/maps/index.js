49e50323d0a6e2f6aad9d2afd604d94414ad34a1
Colleen <colleen.j.mcginnis@gmail.com>
2018-07-09 09:27:25 -0700

    add back temporary redirects

---
+++ src/pages/maps/index.js
@@ -0,0 +1,4 @@
+import { prefixUrl } from '@mapbox/batfish/modules/prefix-url';
+import { createRedirect } from '../../components/create-redirect';
+
+export default createRedirect(prefixUrl('/maps/overview/'));
