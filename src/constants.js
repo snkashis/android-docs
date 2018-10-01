effb933c5770ccde04e95157c6c2ba0dd920e0fb
Langston Smith <langston.smith@mapbox.com>
2018-09-30 22:26:08 -0700

    Nav SDK 0.20.0 release bump + API reference docs (#649)
    * bump
    
    * added api reference docs

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 /* SDK Versions */
 export const JAVA_SDK_VERSION = '4.0.0';
 export const MAP_SDK_VERSION = '6.5.0';
-export const NAVIGATION_VERSION = '0.19.0';
+export const NAVIGATION_VERSION = '0.20.0';
 export const TELEMETRY_VERSION = '3.0.0-beta.1';
 export const CORE_VERSION = '0.2.1';
 export const GESTURES_VERSION = '0.2.0';
