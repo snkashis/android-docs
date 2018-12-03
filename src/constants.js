53e3f13061d1f4695b249619fafb478912ca8b3f
Langston Smith <langston.smith@mapbox.com>
2018-12-03 11:19:52 -0800

    bumped (#739)

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 /* SDK Versions */
 export const JAVA_SDK_VERSION = '4.1.1';
 export const MAP_SDK_VERSION = '6.7.2';
-export const NAVIGATION_VERSION = '0.23.0';
+export const NAVIGATION_VERSION = '0.24.0';
 export const TELEMETRY_VERSION = '4.1.0';
 export const CORE_VERSION = '1.1.0';
 export const VISION_VERSION = '0.1.2';
