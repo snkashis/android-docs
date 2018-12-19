ee5b042d610ffef879ef97b5f887622d2ba290d1
Langston Smith <langston.smith@mapbox.com>
2018-12-19 11:13:05 -0800

    Maps 6.8.0 bump + api ref docs (#781)
    * bump
    
    * added api reference docs

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 /* SDK Versions */
 export const JAVA_SDK_VERSION = '4.3.0';
-export const MAP_SDK_VERSION = '6.7.2';
+export const MAP_SDK_VERSION = '6.8.0';
 export const NAVIGATION_VERSION = '0.25.0';
 export const TELEMETRY_VERSION = '4.2.0';
 export const CORE_VERSION = '1.1.0';
