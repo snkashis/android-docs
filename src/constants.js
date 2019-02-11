e711ae32d55a917b40dd65d514b8ba7bbd423166
Langston Smith <langston.smith@mapbox.com>
2019-02-11 09:21:45 -0800

    bump (#841)

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 /* SDK Versions */
 export const JAVA_SDK_VERSION = '4.3.0';
-export const MAP_SDK_VERSION = '7.1.1';
+export const MAP_SDK_VERSION = '7.1.2';
 export const NAVIGATION_VERSION = '0.28.0';
 export const TELEMETRY_VERSION = '4.2.0';
 export const CORE_VERSION = '1.1.0';
